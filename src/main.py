d68922534b1061c90d60a58c61243d7d94cc19ff
Jacob Terpe Woods <jacobterpewoods@gmail.com>
2025-11-17 20:38:16 +0100

    feat: added animation argument to main script

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
+"""Main module for the project."""
 from secret_sharing import Graph, Topology, Simulation, Visualizer
 from utils.logging import get_colored_logger
 from argparse import ArgumentParser
+from secret_sharing.simulation import Algorithm
 
 argparser = ArgumentParser()
 argparser.add_argument(
@@ -11,22 +13,25 @@ argparser.add_argument(
     help="Topology of the graph. Possible values: RING, STAR, TREE, MESH, FULL",
 )
 argparser.add_argument(
-    "-n", "--num_nodes", type=int, default=10, help="Number of nodes in the graph"
+    "-n", "--num_nodes", type=int, default=10, help="Number of nodes in the graph",
 )
 argparser.add_argument(
-    "-l", "--lower_bound", type=int, default=10, help="Lower bound for initial values"
+    "-l", "--lower_bound", type=int, default=10, help="Lower bound for initial values",
 )
 argparser.add_argument(
-    "-u", "--upper_bound", type=int, default=100, help="Upper bound for initial values"
+    "-u", "--upper_bound", type=int, default=100, help="Upper bound for initial values",
 )
 argparser.add_argument(
-    "-r", "--random_range", type=int, default=100, help="Random range for shares"
+    "-r", "--random_range", type=int, default=100, help="Random range for shares",
 )
 argparser.add_argument(
-    "-v", "--visualize", action="store_true", help="Visualize the graph"
+    "-v", "--visualize", action="store_true", help="Visualize the graph",
 )
 argparser.add_argument(
-    "-p", "--plot", action="store_true", help="Plot the error history"
+    "-p", "--plot", action="store_true", help="Plot the error history",
+)
+argparser.add_argument(
+    "-a", "--animate", action="store_true", help="Animate the convergence",
 )
 args = argparser.parse_args()
 
@@ -38,13 +43,13 @@ graph.apply_shares(random_range=args.random_range)
 
 simulator = Simulation()
 visualizer = Visualizer()
-error_list_sync, iterations_sync, avg_sync = simulator.run_ass_synchronous(graph)
-error_list_async, iterations_async, avg_async = simulator.run_ass_asynchronous(graph)
+res_sync = simulator.run_simulation(graph, Algorithm.SYNCHRONOUS)
+res_async = simulator.run_simulation(graph, Algorithm.ASYNCHRONOUS)
 
-log.info(f"Synchronous Iterations: {iterations_sync}")
-log.info(f"Asynchronous Iterations: {iterations_async}")
-log.info(f"Synchronous Average: {avg_sync}")
-log.info(f"Asynchronous Average: {avg_async}")
+log.info(f"Synchronous Iterations: {res_sync.total_iterations}")
+log.info(f"Asynchronous Iterations: {res_async.total_iterations}")
+log.info(f"Synchronous Average: {res_sync.final_avg}")
+log.info(f"Asynchronous Average: {res_async.final_avg}")
 log.info(f"True Average: {graph.true_avg}")
 
 
@@ -57,11 +62,22 @@ graph_drawing = visualizer.draw_graph(
 
 if args.plot:
     visualizer.plot_convergence(
-        [
-            (error_list_sync, "Synchronous", 1),
-            (error_list_async, "Asynchronous", args.num_nodes),
-        ],
+        [res_sync, res_async],
         title="Convergence",
         save_path="figures/convergence.png",
         show=args.plot,
+    )
+
+if args.animate:
+    visualizer.animate_convergence(
+        graph,
+        res_async,
+        "figures/async_animation.gif",
+        interval=100,
+    )
+    visualizer.animate_convergence(
+        graph,
+        res_sync,
+        "figures/sync_animation.gif",
+        interval=100,
     )
