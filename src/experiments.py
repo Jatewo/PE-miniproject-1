2454c13e2f4b29403486fc9f6f6224dd2ada91da
Jacob Terpe Woods <jacobterpewoods@gmail.com>
2025-11-18 23:14:53 +0100

    feat: Experiment file w/ experiment 1 for bonus questions

---
+++ src/experiments.py
@@ -0,0 +1,59 @@
+"""Module for running experiments for bonus question 1."""
+
+from secret_sharing import Graph, Topology, Simulation, Visualizer
+from secret_sharing.simulation import Algorithm
+from utils.logging import get_colored_logger
+
+log = get_colored_logger(__name__)
+
+
+def experiment_1() -> None:
+    """Compare topologies for additive secret sharing.
+
+    Runs a simulation for each topology and saves convergence plots for comparison.
+
+    Returns:
+        None
+
+    """
+    graphs = [Graph(num_nodes=10, topology=topology) for topology in Topology]
+    simulator = Simulation()
+    visualizer = Visualizer()
+
+    results = []
+
+    for graph in graphs:
+        graph.set_initial_values(range_start=0, range_end=100)
+        graph.apply_shares(random_range=100.0)
+        log.info(f"Running experiment for {graph.topology.name} topology...")
+        res = simulator.run_simulation(graph, Algorithm.SYNCHRONOUS)
+
+        if graph.topology == Topology.FULL:
+            log.debug(res)
+
+        results.append(res)
+
+        log.info(f"Plotting convergence for {graph.topology.name} topology...")
+        visualizer.plot_convergence(
+            [res],
+            save_path=f"figures/experiment_1/synchronous_{graph.topology.name}.png",
+            show=False,
+        )
+
+        log.info(f"Animating convergence for {graph.topology.name} topology...")
+        visualizer.animate_convergence(
+            graph,
+            res,
+            f"figures/experiment_1/synchronous_{graph.topology.name}.gif",
+        )
+
+    visualizer.plot_convergence(
+        results,
+        save_path="figures/experiment_1/synchronous_convergence.png",
+        show=False,
+        y_min=1e-6,
+    )
+
+
+if __name__ == "__main__":
+    experiment_1()
